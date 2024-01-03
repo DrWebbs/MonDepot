369f1e2b28621799489f77b50653a749b16fa656
DrWebbs <nicolas.blazy@gmail.com>
2024-01-03 16:04:06 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,8 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<meta name="robot" content="nofollow" />
+<meta name="verify-v1" content="Qcoq5MC+KkAPdQtLB2UlAVCpOn51zBc1thZq5veZdI4=">
+<META http-equiv="Pragma" CONTENT="no-cache">
+</head><body>
+<img src="images/404.png" >
+</body>
